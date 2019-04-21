1587df55fb7b9a1ac47a33b7d976460b6c9defd7
SilicaGel_SrD <makaron3141@gmail.com>
2019-04-21 10:25:39 +0900

    通常課題

---
+++ Assets/Script/ItemController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour {
+
+	private GameObject ItemLive;
+
+	// Use this for initialization
+	void Start () {
+		// MainCameraのデータを取得
+		this.ItemLive = GameObject.Find( "MainCamera" );
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float cam, item;
+
+		// カメラとアイテムのZ座標を取得
+		cam = this.ItemLive.GetComponent<Transform>().position.z;
+		item = this.GetComponent<Transform>().position.z;
+
+		// カメラより後ろに配置されたら、消去
+		if( item < cam ) {
+			Destroy( this.gameObject );
+		}
+	}
+}
